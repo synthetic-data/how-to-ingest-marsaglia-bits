590df0a8532b19831671cfbfb285123ccd8ea9aa
Alex Fedotov <alxfed@gmail.com>
2018-04-26 18:46:06 -0500

    corrected with comprehension

--- reading_marsaglia.py
+++ reading_marsaglia.py
@@ -1,7 +1,10 @@
 # How to read Marsaglia bit on Linux in python
 
-file_to_read = open('/media/alxfed/toca/bits.01', mode="rb")
-a = file_to_read.read()
+file_to_read = open('/media/alxfed/toca/bits.42', mode="rb")
+a = file_to_read.read(100)
 out = file_to_read.close()
 
-print(int(a[3]))
+printable_bytes = [int(x) for x in a]
+print(printable_bytes[0:9])
+
+# same numbers as in R example
