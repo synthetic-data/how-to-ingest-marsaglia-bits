340c81134d1a984f09a310efabc24913e9dcbb3a
Alex Fedotov <alxfed@gmail.com>
2018-04-26 19:02:30 -0500

    small addition

--- reading_marsaglia.py
+++ reading_marsaglia.py
@@ -5,6 +5,8 @@ a = file_to_read.read(100)
 out = file_to_read.close()
 
 printable_bytes = [int(x) for x in a]
+# or printable_bytes = [int(x) for x in a if x < 10]
+
 print(printable_bytes[0:9])
 
 # same numbers as in R example
