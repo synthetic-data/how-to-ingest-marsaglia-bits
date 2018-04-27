5afa30eea373ae302235eb770fda9e5833232289
Alex Fedotov <alxfed@gmail.com>
2018-04-27 12:55:00 -0500

    filling numpy 1d array

--- filling_numpy_array.py
+++ filling_numpy_array.py
@@ -3,9 +3,14 @@
 import numpy as np
 import supplement as supp
 
-
-A = np.fromfile(supp.file_name(42), dtype=np.uint8, count=-1, sep="")
+# Define a shape, read, then - reshape
+shape = (4, 4, 4, 4)
+A = np.fromfile(
+        supp.file_name(42),
+        dtype = np.uint8,
+        count = 256,
+        sep = ""
+    ).reshape(shape)
 
 print(A)
-print(len(A))
 
