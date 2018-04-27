878a82a82e312a18c23d6c9f5c2439c4bce2944f
Alex Fedotov <alxfed@gmail.com>
2018-04-27 10:15:45 -0500

    a slight polish

--- supplement.py
+++ supplement.py
@@ -7,15 +7,14 @@ FILE_NUMBER_MAX = 60
 
 # pseudo-constants
 FILE_EXTENSION = [str(i).zfill(2) for i in range(FILE_NUMBER_MIN, FILE_NUMBER_MAX + 1)]
+# starts with 0 element and ends with 59, that's why the dance in the function
 
 # pseudo-function
 def file_name(n=1):
-    """
-    if n is in the range of existing files - return the file name, else - except
-    """
-    if n in range(FILE_NUMBER_MIN, FILE_NUMBER_MAX+1):
-        return FILE_BASE + FILE_EXTENSION[n-1]
+    if n in range(FILE_NUMBER_MIN, FILE_NUMBER_MAX+1):  # +1 because...
+        return FILE_BASE + FILE_EXTENSION[n-1]          # -1 because...
     else:
-        raise ValueError('There is no such file in Marsaglia')
+        raise ValueError('There is no such file in Marsaglia set of bits')
+
 
 print(file_name(1))
