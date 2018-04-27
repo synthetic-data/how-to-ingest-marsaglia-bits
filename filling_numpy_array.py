56f769722b83d65367c7356b1f74ac44ffa1b543
Alex Fedotov <alxfed@gmail.com>
2018-04-27 10:57:43 -0500

    filling numpy 1d array

--- filling_numpy_array.py
+++ filling_numpy_array.py
@@ -3,13 +3,9 @@
 import numpy as np
 import supplement as supp
 
-#file_to_read = open(supp.file_name(1), mode="rb")
 
 A = np.fromfile(supp.file_name(42), dtype=np.uint8, count=-1, sep="")
-#a = file_to_read.read()
-#out = file_to_read.close()
 
 print(A)
 print(len(A))
 
-pass
