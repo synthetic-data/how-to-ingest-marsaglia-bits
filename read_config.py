aece06c7d2c51367342a95559ce9734f4df819de
Alex Fedotov <alxfed@gmail.com>
2018-04-28 09:55:00 -0500

    parse config

--- read_config.py
+++ read_config.py
@@ -10,12 +10,23 @@ parser = ConfigParser()
 
 if not parser.read(CONFIG_FILE_NAME):           # no file yet, create one
     file_to_write = open(CONFIG_FILE_NAME, mode="x")
-    parser.add_section('data_ndarray')
-    parser.set('data_ndarray', 'x', '3')
-    parser.set('data_ndarray', 'y', '4')
-    parser.set('data_ndarray', 'z', '5')
+    parser.add_section('data_array')
+    parser.set('data_array', 'x', '3')
+    parser.set('data_array', 'y', '4')
+    parser.set('data_array', 'z', '5')
     parser.write(file_to_write)
     out = file_to_write.close()
 
-else:                           # file read, start parsing
-    parser.read(CONFIG_FILE_NAME)
+else:                           # file read, get out of the else and start parsing
+    pass
+
+
+parser.read(CONFIG_FILE_NAME)
+s1 = parser.getint('data_array', 'x')
+s2 = parser.getint('data_array', 'y')
+s3 = parser.getint('data_array', 'z')
+
+
+shape = (s1, s2, s3)
+
+#print(shape)
